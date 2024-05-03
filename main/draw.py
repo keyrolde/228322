fcdbcd30f8b4c01e72c5337fd3a208e6f4b7a818
keyrolde <12345n04@mail.ru>
2024-05-03 17:19:33 +0700

    готовый продукт

--- main/draw.py
+++ main/draw.py
@@ -16,24 +16,25 @@ class Risovalka(Frame):
         self.btn.pack()
         self.figure = Figure([Dot(), Dot(), Dot()])
         self.canvas = Canvas(root, width=400, height=400, bg="gray", cursor="pencil")
+        self.triangle = None
         self.do_triangle()
+        self.en1 = ttk.Entry()
+        self.en1.pack()
+        self.en2 = ttk.Entry()
+        self.en2.pack()
+
 
 
     def do_triangle(self):
-        self.canvas.create_polygon(self.figure.get_dot_list(), width=2, fill="white", outline="black")
+        self.triangle = self.canvas.create_polygon(self.figure.get_dot_list(), width=2, fill="white", outline="black")
         self.canvas.pack()
-    def click_button(self):
-        print("авд")
-        self.canvas.delete("all")
-        self.figure = Figure([Dot(), Dot(), Dot()])
-        self.do_triangle()
+       def click_button(self):
+        self.canvas.move(self.triangle, self.en1.get(), self.en2.get())
 
 
 a = Risovalka()
 
-en1 = ttk.Entry()
-# en1.place(x=10, y=10)
-en1.pack(padx=10, pady=10)
+
 
 
 
